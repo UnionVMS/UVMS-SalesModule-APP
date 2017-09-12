f9a07290d1a38948342f4fac77847e9feb137f4f
stihft <stijn.hooft@lv.vlaanderen.be>
2017-09-12 13:53:41 +0200

    Fix transaction issue Sprint Backlog Item #143739 - Wanneer een asset niet bestaat, laadt de lijst van Sales notes niet

--- service/src/main/java/eu/europa/ec/fisheries/uvms/sales/service/bean/EcbProxyServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/sales/service/bean/EcbProxyServiceBean.java
@@ -14,11 +14,14 @@ import org.joda.time.DateTime;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
 import java.math.BigDecimal;
 
 @Stateless
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class EcbProxyServiceBean implements EcbProxyService {
 
     @EJB
