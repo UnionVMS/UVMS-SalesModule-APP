0bad5532712bfdea056fcd10d8853cc6217146c5
stihft <stijn.hooft@lv.vlaanderen.be>
2017-10-26 16:30:32 +0200

    cleanup unused code and move message constant to uvms-commons

--- message/src/main/java/eu/europa/ec/fisheries/uvms/sales/message/constants/SalesMessageConstants.java
+++
@@ -1,8 +0,0 @@
-package eu.europa.ec.fisheries.uvms.sales.message.constants;
-
-//TODO: move to MessageConstants in UVMS-commons, when we have Git
-public interface SalesMessageConstants {
-
-
-    String QUEUE_ECB_PROXY = "java:/jms/queue/UVMSSalesEcbProxy";
-}
