015e0c55ed8aa42b113b4e88b9602992984bc954
KRISCH <kris.schockaert@lv.vlaanderen.be>
2018-02-27 14:37:47 +0100

    Product Backlog Item #147475 Opzet Arquillian framework voor Sales Move sales service integration tests to dedicated sales service integration tests module. Task #153117 Verplaats Sales Arquillian framework naar specifieke integration test jar

---
+++ service-integration-tests/src/test/java/eu/europa/ec/fisheries/uvms/sales/service/integrationtest/deployment/StandardTestDeployment.java
@@ -0,0 +1,26 @@
+package eu.europa.ec.fisheries.uvms.sales.service.integrationtest.deployment;
+
+import eu.europa.ec.fisheries.uvms.sales.service.integrationtest.deployment.factory.TestDeploymentFactory;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+public abstract class StandardTestDeployment {
+
+    private static final String TEST_ARCHIVE_NAME = "sales_service_standard_test";
+
+    @Deployment(name = "salesservice", order = 1)
+    public static WebArchive createStandardDeployment() {
+        WebArchive standardTestWebArchive = TestDeploymentFactory.createBasicTestDeployment(TEST_ARCHIVE_NAME);
+
+        // Alternative ejb bean
+        standardTestWebArchive.addAsWebInfResource("ejb-jar-test.xml", "ejb-jar.xml");
+
+        // Test data
+        standardTestWebArchive.addAsResource("test-data/report_original.txt", "test-data/report_original.txt");
+        standardTestWebArchive.addAsResource("test-data/before_corrections.txt", "test-data/before_corrections.txt");
+        standardTestWebArchive.addAsResource("test-data/corrections.txt", "test-data/corrections.txt");
+
+        return standardTestWebArchive;
+    }
+
+}
